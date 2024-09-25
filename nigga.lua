cc1cd0e68f55b557e8afe5891c1aece77a9022bf
ossy69 <jjzcuh@gmail.com>
2024-09-25 09:23:11 -0400

    Update nigga.lua

--- nigga.lua
+++ nigga.lua
@@ -1,12 +1,12 @@
-{FrostDragon = {4470748644, "Legendary"},
+{Frost Dragon = {4470748644, "Legendary"},
 Kangaroo = {4708551306, "Legendary"},
 Turtle = {4708551322, "Legendary"},
 Giraffe = {3409443710, "Legendary"},
 Parrot = {3743647944, "Legendary"},
 Owl = {4440866649, "Legendary"},
-ShadowDragon = {4115248712, "Legendary"},
+Shadow Dragon = {4115248712, "Legendary"},
 Crow = {4440866299, "Legendary"},
-StrawbarryBat = {11506045996, "Legendary"},
+Strawberry Bat Dragon = {11506045996, "Legendary"},
 Cow = {4440866423, "Rare"},
-BatDragon = {4184878149, "Legendary"},
-EvilUnicorn = {4184878050, "Legendary"}}
+Bat Dragon = {4184878149, "Legendary"},
+Evil Unicorn = {4184878050, "Legendary"}}
